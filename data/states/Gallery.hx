d71c6cb7b26de274cec1e5c62e7273a36f0f4f7d
Eren Kaya <erenkaya6023544@gmail.com>
2026-02-21 18:52:58 +0300

    got to like be forced to do it :(

--- data/states/Gallery.hx
+++ data/states/Gallery.hx
@@ -1,14 +1,36 @@
 import flixel.text.FlxTextBorderStyle;
+import flixel.FlxObject;
 import flixel.text.FlxTextAlign;
 
 var bg:FlxSprite = new FlxSprite(0, 0);
 var boomboom:FunkinSprite = new FunkinSprite(0, 0);
+
+var camFollow:FlxObject = new FlxObject(0, 0, 1, 1);
+var curSelected:Int = 0;
+
+var boomed:Bool = false;
+
+var goonFolderShit = [
+    "tord1",
+    "tord2",
+    "tord3",
+    "tord4",
+    "tord5",
+    "tord6",
+    "tord7",
+    "Screenshot_2026-02-15_014923",
+    "Screenshot_2026-02-15_014930",
+    "Screenshot_2026-02-15_014940"
+];
+
+var goonerrrrr:FlxGroup = new FlxGroup();
 
 function create()
 {
     add(bg).loadGraphic(Paths.image("game/mainmenu/browserShit"));
     bg.setGraphicSize(FlxG.width, FlxG.height);
     bg.updateHitbox();
+    bg.scrollFactor.set();
     bg.antialiasing = true;
     bg.screenCenter();
 
@@ -18,6 +40,25 @@ function create()
     title.antialiasing = true;
     title.scrollFactor.set();
     add(title);
+
+    add(camFollow);
+    add(goonerrrrr);
+
+    for (i => image in goonFolderShit)
+    {
+        var goon = new FlxSprite(0, 0).loadGraphic(Paths.image("game/goonstash/" + image));
+        goon.setGraphicSize(FlxG.width / 1.7, FlxG.height / 1.7);
+        goon.updateHitbox();
+        goon.ID = i;
+        goon.antialiasing = true;
+        goon.screenCenter();
+        goon.x = 0 + (i * (640 + 200));
+        goonerrrrr.add(goon);
+    }
+
+    FlxG.camera.follow(camFollow, null, 0.06);
+    FlxG.mouse.visible = true;
+    changeItem(curSelected);
 }
 
 function update(elapsed:Float)
@@ -27,9 +68,31 @@ function update(elapsed:Float)
         FlxG.switchState(new MainMenuState());
     }
 
+    if (controls.LEFT_P)
+    {
+        curSelected = (curSelected - 1 + goonerrrrr.length) % goonerrrrr.length;
+        changeItem(curSelected);
+    }
+
+    if (controls.RIGHT_P)
+    {
+        curSelected = (curSelected + 1) % goonerrrrr.length;
+        changeItem(curSelected);
+    }
+
     if (FlxG.mouse.pressed)
     {
-        //doBoomBoom(FlxG.mouse.x - 32.5, FlxG.mouse.y - 52.5);
+        if (FlxG.mouse.overlaps(goonerrrrr.members[curSelected]) && !boomed)
+        {
+            boomed = true;
+            boomboomthatshit(curSelected);
+
+            FlxTween.tween(goonerrrrr.members[curSelected], {alpha: 0}, 0.5, {ease: FlxEase.quadOut, onComplete: function()
+            {
+                goonerrrrr.members[curSelected].destroy();
+                boomed = false;
+            }});
+        }
     }
 }
 
@@ -37,6 +100,7 @@ function doBoomBoom(x, y)
 {
     var boomboom = new FunkinSprite(x, y).loadGraphic(Paths.image("game/explosion"));
     boomboom.frames = Paths.getFrames("game/explosion");
+    boomboom.setGraphicSize(Std.int(boomboom.width * 16));
     boomboom.addAnim("explosion", "explosion boom", 24);
     boomboom.playAnim("explosion");
     add(boomboom);
@@ -44,8 +108,36 @@ function doBoomBoom(x, y)
     be = FlxG.sound.load(Paths.sound("badexplosion"));
     be.play();
 
-    if (boomboom.isAnimFinished())
+    if (boomboom.isAnimFinished() == "explosion")
     {
         boomboom.destroy();
     }
+}
+
+function changeItem(huh:Int = 0)
+{
+    goonerrrrr.forEach(function(spr:FlxSprite)
+    {
+        if (spr.ID == curSelected)
+        {
+            FlxTween.tween(spr, {alpha: 1}, 0.25, {ease: FlxEase.quadOut});
+
+            var mid = spr.getGraphicMidpoint();
+			camFollow.setPosition(mid.x - spr.width / 1.25, mid.y - spr.height / 1.25);
+			mid.put();
+        } else if (!boomed) {
+            FlxTween.tween(spr, {alpha: 0.4}, 0.25, {ease: FlxEase.quadOut});
+        }
+    });
+}
+
+function boomboomthatshit(id)
+{
+    goonerrrrr.forEach(function(spr:FlxSprite)
+    {
+        if (spr.ID == id)
+        {
+            doBoomBoom(FlxG.mouse.x - 32.5, FlxG.mouse.y - 52.5);
+        }
+    });
 }
