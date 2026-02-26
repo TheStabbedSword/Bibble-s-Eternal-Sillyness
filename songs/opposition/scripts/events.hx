4290f19424bf2ba82d1ec624d25080dab01fb569
Eren Kaya <erenkaya6023544@gmail.com>
2026-02-26 22:30:01 +0300

    changingsss

--- songs/opposition/scripts/events.hx
+++ songs/opposition/scripts/events.hx
@@ -20,4 +20,15 @@ function postUpdate(elapsed:Float)
 
     camHUD.angle = Math.sin(Conductor.songPosition / 1000) * 4;
     camHUD.zoom = 1 + (Math.cos(Conductor.songPosition / 1000) * 0.04);
+
+    playerStrums.forEach(function(spr:FlxSprite)
+	{
+		spr.x = ((FlxG.width / 2) - (spr.width / 2)) + (Math.sin((elapsedtime * 0.3) + spr.ID + 1) * (FlxG.width * 0.4));
+		spr.y = ((FlxG.height / 2) - (spr.height / 2)) + (Math.sin(((elapsedtime * 0.3) + spr.ID) * 3) * (FlxG.height * 0.2));
+	});
+	cpuStrums.forEach(function(spr:FlxSprite)
+	{
+		spr.x = ((FlxG.width / 2) - (spr.width / 2)) + (Math.sin((elapsedtime * 0.3) + spr.ID + 1.5) * (FlxG.width * 0.4));
+		spr.y = ((FlxG.height / 2) - (spr.height / 2)) + (Math.sin((((elapsedtime * 0.3) + spr.ID) * -3) + 0.5) * (FlxG.height * 0.2));
+	});
 }
